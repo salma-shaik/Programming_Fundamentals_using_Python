e6fcab1666104cd98afca524fdf082ee973a9b86
Salma Shaik <salmashaik1611@gmail.com>
2024-01-24 15:10:11 -0500

    string slicing, formatting

--- strings.py
+++ strings.py
@@ -51,7 +51,7 @@ print(first_name + ' ' + last_name + ':' + str(age))
 
 # String multiplication - can multiple a string with an int
 print("hello"*3)
-'''
+
 # Accessing string characters - a string is just a sequence of characters
 name = "Jane Doe"
 print(name[2])
@@ -60,6 +60,37 @@ print(name[2])
 # Retrieving the character at a given index
 print(name.index('o')) # returns 6
 print(name.index('e')) # returns the index of first occurrence
+'''
+
+
+###### STRING SLICING ####
+emp_name ="Jane Doe"
+print(emp_name[2:6])
+print(emp_name[:4])
+print(emp_name[2:])
+print(emp_name[-4:-1])
+print(emp_name[1:6:2])
+
+print(emp_name.count('e'))
+
+print(emp_name.find('Doe'))
+
+emp_name = emp_name.replace('Jane', 'John')
+
+# Membership Test
+print('oh' in emp_name)
+
+# String Formatting
+student_name = "Alice"
+score = 87
+
+print(student_name + ': ' +str(score))
+print("Name: {} Score: {}".format(student_name, score))
+# f-strings
+print(f'Name: {student_name} Score: {score}')
+print(f'10 times 3 is {10*3}')
+
+
 
 
 
